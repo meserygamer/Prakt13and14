7e312cd9c98e0db7b324123a61aa330bcf40547b
MeseryGG <meserygamer@gmail.com>
2024-02-05 12:51:06 +0300

    Добавление страницы списка курсов

---
+++ TeachersTestAppWithLogging/ViewModels/CourcesListUCViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using ReactiveUI;
+
+namespace TeachersTestAppWithLogging.ViewModels
+{
+	public class CourcesListUCViewModel : ReactiveObject
+	{
+	}
+}
