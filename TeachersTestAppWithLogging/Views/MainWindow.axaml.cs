c8c809a1b63068141cfa3e464de10ab1c6a72b05
MeseryGG <meserygamer@gmail.com>
2024-02-08 18:12:40 +0300

    Переподключено

--- TeachersTestAppWithLogging/Views/MainWindow.axaml.cs
+++ TeachersTestAppWithLogging/Views/MainWindow.axaml.cs
@@ -6,6 +6,17 @@ namespace TeachersTestAppWithLogging.Views
     {
         public MainWindow()
         {
+            //using (Vorobiew2Context DB = new())
+            //{
+            //    DB.Cources.Add(new Cource {Cource1 = "Введение в программирование на Си", HoursNumber = 122});
+            //    DB.Cources.Add(new Cource { Cource1 = "Математический анализ", HoursNumber = 75});
+            //    DB.Cources.Add(new Cource { Cource1 = "История и культура стран Востока", HoursNumber = 38});
+            //    DB.Cources.Add(new Cource { Cource1 = "Финансовый анализ и управление рисками", HoursNumber = 99});
+            //    DB.Cources.Add(new Cource { Cource1 = "Современная философия и этика", HoursNumber = 56});
+            //    DB.Cources.Add(new Cource { Cource1 = "Международные отношения и глобальная политика", HoursNumber = 27});
+            //    DB.Cources.Add(new Cource { Cource1 = "Теория и методы машинного обучения", HoursNumber = 14});
+            //    DB.SaveChanges();
+            //}
             InitializeComponent();
         }
     }
